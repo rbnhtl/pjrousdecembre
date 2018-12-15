1ec722d1c86e3f593cff5ddc64e3d6a0e5766b61
Robin <robin.hortala@gmail.com>
2018-12-15 19:44:38 +0100

    Ajout Matiere Personnel et Role en BD

--- src/Etudiant.php
+++ src/Etudiant.php
@@ -4,6 +4,7 @@
  * @author robin.hortala
  *
  */
+
  /**
   * @Entity @Table(name="etudiant")
   **/
@@ -11,16 +12,16 @@ class Etudiant
 {
 	/** @Id @Column(type="string", length=13) **/
     private $ine;
-	
+
 	/** @Column(type="integer") **/
-    private $id_groupe; 
+    private $id_groupe;
 
 	/** @Column(type="string") **/
     private $nom;
 
 	/** @Column(type="string") **/
     private $prenom;
-    
+
     // Constructeur de la classe
     public function Etudiant($ine,$id_groupe,$nom,$prenom)
     {
@@ -29,49 +30,26 @@ class Etudiant
         $this->nom = $nom;
         $this->prenom = $prenom;
     }
-    
-    /* Getters et Setters */
 
-    public function getIne()
-    {
-        return $this->ine;
-    }
-	
-    public function setIne($ine)
-    {
-        $this->ine = $ine;
-    }
+    /* Getter et Setter INE */
+    public function getIne(){ return $this->ine; }
 
-    public function getNom()
-    {
-        return $this->nom;
-    }
- 
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-    }
+    public function setIne($ine){ $this->ine = $ine; }
 
-    public function getPrenom()
-    {
-        return $this->prenom;
-    }
+    /* Getter et Setter NOM */
+    public function getNom(){ return $this->nom; }
 
-    public function setPrenom($prenom)
-    {
-        $this->prenom = $prenom;
-    }
+    public function setNom($nom){ $this->nom = $nom; }
 
-    public function getIdGroupe()
-    {
-        return $this->id_groupe;
-    }
- 
-    public function setIdGroupe($id_groupe)
-    {
-        $this->id_groupe = $id_groupe;
-    }
-    
+    /* Getter et Setter PRENOM */
+    public function getPrenom() { return $this->prenom; }
+
+    public function setPrenom($prenom){ $this->prenom = $prenom; }
+
+    /* Getter et Setter ID_GROUPE */
+    public function getIdGroupe() { return $this->id_groupe; }
+
+    public function setIdGroupe($id_groupe){ $this->id_groupe = $id_groupe; }
 }
 
 ?>
