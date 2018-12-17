24b12ebd7870e43d47ad6b6d2713bcbeeffc93fd
Robin <robin.hortala@gmail.com>
2018-12-17 23:02:52 +0100

    Correction d'import étudiant
    L'import étudiant fonctionne, mais il faut que des groupes existent, si ce n'est pas le cas, aucune erreur n'est retournée

--- Utils/import_etudiant.php
+++ Utils/import_etudiant.php
@@ -8,16 +8,17 @@
 		$db = new PDO("mysql:host=localhost;dbname=gestioneleve;charset=utf8","root","root");
 		$fp = file_get_contents($_FILES[$fichier]["tmp_name"]);
 		//remplis la table etudiant avec les le fichier csv
-		$reqEtudiant = $db->prepare("INSERT INTO etudiant(ine,id_groupe,nom,prenom) VALUES (:ine, :nom, :prenom, :groupe)");
+		$reqEtudiant = $db->prepare("INSERT INTO etudiant (ine,nom,prenom,idGroupe) VALUES (:ine, :nom, :prenom, :groupe)");
 		$liste = explode(" ",$fp);
 		foreach($liste as $etudiant) {
 			$champs = explode("|",$etudiant);
-			$reqEtudiant->bindParam(":ine",$champs[0]);
-			$reqEtudiant->bindParam(":nom",$champs[1]);
-			$reqEtudiant->bindParam(":prenom",$champs[2]);
-			$reqEtudiant->bindParam(":groupe",$champs[3]);
+			// $reqEtudiant->bindParam(":ine",$champs[0]);
+			// $reqEtudiant->bindParam(":nom",$champs[1]);
+			// $reqEtudiant->bindParam(":prenom",$champs[2]);
+			// $reqEtudiant->bindParam(":groupe",$champs[3]);
 			try {
-				$reqEtudiant->execute();
+				$reqEtudiant->execute(array(':ine' => $champs[0], ':nom' => $champs[1], ':prenom' => $champs[2], ':groupe' => intval($champs[3])));
+				echo("Etudiant ajouté en BD<br>");
 			} catch(PDOException $Exception) {
 				echo $Exception->getMessage( );
 				throw new MyDatabaseException( $Exception->getMessage( ), (int)$Exception->getCode( ) );
