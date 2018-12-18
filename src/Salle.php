a0053193aaeda724fe3b64c240c1b78b377a6c3a
Robin <robin.hortala@gmail.com>
2018-12-18 22:17:05 +0100

    Fix #4 Ajout de la description de la salle

--- src/Salle.php
+++ src/Salle.php
@@ -13,16 +13,25 @@ class Salle
     /** @Id @Column(type="string",length=5) **/
     private $num;
 
+    /** @Column(type="string") **/
+    private $description;
+
     // Constructeur de la classe
     public function Salle($num)
     {
         $this->num = $num;
+        $this->description = $description;
     }
 
     /* Getter et Setter NUM */
     public function getNum(){ return $this->num; }
 
     public function setNum($num){ $this->num = $num; }
+
+    /* Getter et Setter DESCRIPTION */
+    public function getDescription(){ return $this->description; }
+
+    public function setDescription($description){ $this->description = $description; }
 }
 
 ?>
