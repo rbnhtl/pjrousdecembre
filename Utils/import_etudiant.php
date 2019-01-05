16ef3d75fb90bed1845a3961e4baa76dd80e4a9e
Robin <robin.hortala@gmail.com>
2019-01-05 17:19:17 +0100

    Import etudiant utilise ORM

--- Utils/import_etudiant.php
+++ Utils/import_etudiant.php
@@ -1,29 +1,46 @@
 <?php
 
+    // On récupère l'entity manager de l'orm doctrine
+	require_once "../bootstrap.php";
+	
+	include "../src/Etudiant.php";
+	include "../src/Appartient.php";
+	include "../DAO/groupeDAO.php";
+
 	$fichier = $_FILES["userfile"]["name"];
 	if($fichier) {
 		importEtudiant("userfile");
 	}
 	function importEtudiant($fichier) {
+		global $em;
+
 		$db = new PDO("mysql:host=localhost;dbname=gestioneleve;charset=utf8","root","root");
 		$fp = file_get_contents($_FILES[$fichier]["tmp_name"]);
-		//remplis la table etudiant avec les le fichier csv
-		$reqEtudiant = $db->prepare("INSERT INTO etudiant (ine,nom,prenom,idGroupe) VALUES (:ine, :nom, :prenom, :groupe)");
+
 		$liste = explode(" ",$fp);
 		foreach($liste as $etudiant) {
 			$champs = explode("|",$etudiant);
-			// $reqEtudiant->bindParam(":ine",$champs[0]);
-			// $reqEtudiant->bindParam(":nom",$champs[1]);
-			// $reqEtudiant->bindParam(":prenom",$champs[2]);
-			// $reqEtudiant->bindParam(":groupe",$champs[3]); 
 			try {
-				$reqEtudiant->execute(array(':ine' => $champs[0], ':nom' => $champs[1], ':prenom' => $champs[2], ':groupe' => intval($champs[3])));
-				echo("Etudiant ajouté en BD<br>");
+
+				// Création d'un nouvel étudiant
+				$newEtudiant = new Etudiant($champs[0], $champs[1], $champs[2]);
+				$groupe = findGroupe($champs[3]);
+				$em->persist($newEtudiant);
+				$em->flush();
+
+				// Création de la relation appartient liée à l'étudiant
+				$newAppartient = new Appartient();
+				$newAppartient->setGroupe($groupe);
+				$newAppartient->setEtud($newEtudiant);
+				$em->persist($newAppartient);
 			} catch(PDOException $Exception) {
 				echo $Exception->getMessage( );
 				throw new MyDatabaseException( $Exception->getMessage( ), (int)$Exception->getCode( ) );
 			}
 
 		}
+		$em->flush();
+
+		echo("Etudiants ajoutés en BD<br>");
 	}
 ?>
